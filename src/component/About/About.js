d20ce52e16caaa0b5b3f2bdf06f3359ebf2b0e29
Shirinnaz <90415981+Shirinnaz@users.noreply.github.com>
2022-04-15 10:30:14 +0300

    about added

---
+++ src/component/About/About.js
@@ -0,0 +1,46 @@
+import React ,{useState} from "react";
+import '../About/About.css';
+import HeaderBlack from '../HeaderBlack/HeaderBlack';
+import ImageHeader from "../ImageHeader/ImageHeader";
+import danilov1 from '../../Images/danilov1.png';
+import danilov2 from '../../Images/danilov2.png'
+import VideoPart from "../VideoPart/VideoPart";
+import Footer from "../Footer/Footer";
+const About = ()=>{
+    const [text , setText]=useState(false)
+    const [border ,setBorder]=useState(false)
+    return(
+        <div>
+             <HeaderBlack/>
+             <div className="aboutBottom">
+                   <ImageHeader title={'Home  /  About us '} titleH1={'Abous us'} setBorder={!border} setText={!text} imagetext={'Emalatxana'}/>
+                   <div className="danilovimg">
+                       <div className="danilov1img">
+                           <p className="odio">Vitae, odio adipiscing aenean tortor diam, vitae amet volutpat urna</p>
+                           <img src={danilov1}/>
+                           <p className="ante">Vitae, odio adipiscing aenean tortor diam, vitae amet volutpat urna. 
+                               Id aliquam ante eu tellus tristique quam magna non. 
+                                sapien purus massa neque nibh quis tempus natoque in.
+                                 Eget est amet tellus justo faucibus.</p>
+                       </div>
+                       <div className="danilov2img">
+                       <p className="ante">Vitae, odio adipiscing aenean tortor diam,
+                            vitae amet volutpat urna. Id aliquam ante eu tellus tristique
+                             quam magna non. Tortor sapien purus massa neque nibh quis tempus 
+                             natoque in. Eget est amet tellus justo faucibus.</p>
+                           <img src={danilov2}/>
+                           <p className="ante">Vitae, odio adipiscing aenean tortor diam, vitae amet volutpat urna. 
+                               Id aliquam ante eu tellus tristique quam magna non. 
+                                sapien purus massa neque nibh quis tempus natoque in.
+                                 Eget est amet tellus justo faucibus.</p>
+                       </div>
+                       
+                   </div>
+                   <hr/>
+             </div>
+           <VideoPart/>
+           <Footer/>
+        </div>
+    )
+}
+export default About
